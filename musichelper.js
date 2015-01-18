da10912933f42efd660b7d2824cb133f4df1ef55
austincarvey <austin@carvey.me>
2015-01-18 04:30:01 -0500

    Created music functions

---
+++ musichelper.js
@@ -0,0 +1,19 @@
+var unlike = function(){
+$(".thumbDownButton").click()
+}
+
+var like = function(){
+$(".thumbUpButton").click()
+}
+
+var pause = function() {
+$(".pauseButton").click()
+}
+
+var play = function(){
+$(".playButton").click()
+}
+
+var skip = function(){
+$(".skipButton").click()
+}
